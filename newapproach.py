0782afd98f6dc47a16f90d0c6a82fc58a25a64b6
Lochie Ferrier <lochieferrier@gmail.com>
2016-11-20 16:33:03 -0500

    Two stage example, not valid at reasonable dV

--- newapproach.py
+++ newapproach.py
@@ -4,18 +4,19 @@ from gpkit.tools import te_exp_minus1
 from gpkit.feasibility import feasibility_model
 n_stages = 2
 
-dV_requirement = Variable("dV_requirement",100,"m/s")
+dV_requirement = Variable("dV_requirement",4000,"m/s")
 dV = VectorVariable(n_stages,"dV","m/s")
 
-Isp = VectorVariable(n_stages,"Isp",[450,450],"s") #Value from wiki on specific impulse
-theta_fuel = VectorVariable(n_stages,"theta_fuel",[0.9,0.9],"-")
+Isp = VectorVariable(n_stages,"Isp",[282,348],"s") #Value from wiki on specific impulse
+theta_fuel = VectorVariable(n_stages,"theta_fuel",[0.97,0.95],"-")
 z = VectorVariable(n_stages,"z","-")
 m_fuel = VectorVariable(n_stages,"m_fuel","kg")
-m_zfw = VectorVariable(n_stages,"m_zfw",[10,1],"kg")
+m_payload = Variable("m_payload",100,"kg")
+m_zfw = VectorVariable(n_stages,"m_zfw","kg")
 m_dot = VectorVariable(n_stages,"m_dot","kg/s")
 v_exhaust_effective = VectorVariable(n_stages,"v_exhaust_effective","m/s")
 F_thrust = VectorVariable(n_stages,"F_thrust","N")
-
+total_mass = VectorVariable(n_stages,"total_mass","kg")
 m_total = Variable("m_total","kg")
 
 g = Variable("g",9.8,"m/s/s")
@@ -26,20 +27,34 @@ for stage in range(n_stages):
 		Isp[stage] == v_exhaust_effective[stage]/g,
 		m_dot[stage] == F_thrust[stage]/(g*Isp[stage]),
 		z[stage] >= (dV[stage]+g*(m_fuel[stage]/m_dot[stage]))/v_exhaust_effective[stage],
-		theta_fuel[stage] == m_fuel[stage]/m_zfw[stage],
-		theta_fuel[stage] >= te_exp_minus1(z[stage],5),
+		theta_fuel[stage] >= te_exp_minus1(z[stage],5)
 	]
+	if stage == 0:
+		constraints+=[F_thrust[stage]/g >= m_fuel[0] + m_zfw[0] + m_fuel[1] + m_zfw[1] + m_payload,
+					  total_mass[stage] >= m_fuel[stage]+m_fuel[stage+1]+m_zfw[stage]+m_zfw[stage+1]+m_payload,
+					  theta_fuel[stage] == m_fuel[stage]/total_mass[stage]]
+	if stage == 1:
+		constraints+=[F_thrust[stage]/g >= m_fuel[stage] + m_zfw[stage],
+					total_mass[stage] >= m_fuel[stage]+m_zfw[stage] + m_payload,
+					theta_fuel[stage] == m_fuel[stage]/total_mass[stage]]
 
 constraints+=[
-			m_total >= m_zfw[0] + m_fuel[0] + m_zfw[1] + m_fuel[1]]
+			m_total >= m_zfw[0] + m_fuel[0] + m_zfw[1] + m_fuel[1] + m_payload]
 
 with gpkit.SignomialsEnabled():
 	constraints+=[
 		dV_requirement <= dV[0] + dV[1]
 	]
-objective = m_total
 
+objective = m_fuel[0] + m_fuel[1]
+# objective = 1/dV[0] + 1/dV[1]
 m = Model(objective,constraints)
 # so2 = feasibility_model(m.gp(),"max")
 sol = m.localsolve(verbosity=1)
 print sol.table()
+print (1/sol['cost'])
+#
+# m.substitutions.update({dV_requirement:('sweep', [400,800,1200])})
+# a = m.localsolve(printing='false')
+#
+# print a.table()
