95463791d46d3831d5fe5975a8ec1f5d1e8434bb
Lochie Ferrier <lochieferrier@gmail.com>
2016-11-20 15:46:19 -0500

    Two stage example, small values

--- newapproach.py
+++ newapproach.py
@@ -1,37 +1,45 @@
 import gpkit
-from gpkit import Variable, Model
+from gpkit import Variable, VectorVariable, Model
 from gpkit.tools import te_exp_minus1
 from gpkit.feasibility import feasibility_model
+n_stages = 2
 
 dV_requirement = Variable("dV_requirement",100,"m/s")
-dV_total = Variable("dV_total","m/s")
+dV = VectorVariable(n_stages,"dV","m/s")
 
-Isp = Variable("Isp",450,"s") #Value from wiki on specific impulse
-theta_fuel = Variable("theta_fuel",0.9,"-")
-z = Variable("z","-")
-m_fuel = Variable("m_fuel","kg")
-m_zfw = Variable("m_zfw",10,"kg")
+Isp = VectorVariable(n_stages,"Isp",[450,450],"s") #Value from wiki on specific impulse
+theta_fuel = VectorVariable(n_stages,"theta_fuel",[0.9,0.9],"-")
+z = VectorVariable(n_stages,"z","-")
+m_fuel = VectorVariable(n_stages,"m_fuel","kg")
+m_zfw = VectorVariable(n_stages,"m_zfw",[10,1],"kg")
+m_dot = VectorVariable(n_stages,"m_dot","kg/s")
+v_exhaust_effective = VectorVariable(n_stages,"v_exhaust_effective","m/s")
+F_thrust = VectorVariable(n_stages,"F_thrust","N")
+
 m_total = Variable("m_total","kg")
-m_dot = Variable("m_dot","kg/s")
-v_exhaust_effective = Variable("v_exhaust_effective","m/s")
 
 g = Variable("g",9.8,"m/s/s")
-F_thrust = Variable("F_thrust","N")
 
+constraints = []
+for stage in range(n_stages):
+	constraints += [
+		Isp[stage] == v_exhaust_effective[stage]/g,
+		m_dot[stage] == F_thrust[stage]/(g*Isp[stage]),
+		z[stage] >= (dV[stage]+g*(m_fuel[stage]/m_dot[stage]))/v_exhaust_effective[stage],
+		theta_fuel[stage] == m_fuel[stage]/m_zfw[stage],
+		theta_fuel[stage] >= te_exp_minus1(z[stage],5),
+	]
 
-constraints = [
-	Isp == v_exhaust_effective/g,
-	m_dot == F_thrust/(g*Isp),
-	z >= (dV_total+g*(m_fuel/m_dot))/v_exhaust_effective,
-	theta_fuel == m_fuel/m_zfw,
-	theta_fuel >= te_exp_minus1(z,5),
-	dV_total >= dV_requirement,
-	m_total >= m_zfw + m_fuel
-]
-# with gpkit.SignomialsEnabled():
+constraints+=[
+			m_total >= m_zfw[0] + m_fuel[0] + m_zfw[1] + m_fuel[1]]
+
+with gpkit.SignomialsEnabled():
+	constraints+=[
+		dV_requirement <= dV[0] + dV[1]
+	]
 objective = m_total
 
 m = Model(objective,constraints)
-so2 = feasibility_model(m.gp(),"max")
-sol = m.solve(verbosity=1)
+# so2 = feasibility_model(m.gp(),"max")
+sol = m.localsolve(verbosity=1)
 print sol.table()
